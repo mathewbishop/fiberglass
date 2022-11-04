d241a78c338beec11a3fe30da07950577249ba21
mathewbishop <39043451+mathewbishop@users.noreply.github.com>
2022-11-04 02:21:52 +0000

    logo file upload; WIP favicon file upload

--- routes/glass_settings.js
+++ routes/glass_settings.js
@@ -18,100 +18,94 @@ router.get('/', authCheck, function (req, res, next) {
   /* Read Config */
   glass_config = json_file.readFileSync('config/glass_config.json')
 
+  input = '<h2>Configuration</h2>'
+  input += '<br>'
+
   /* Leases File */
-  input = template_render.form_input(
+  input += template_render.form_input(
     'Leases File',
-    '<input type="input" class="form-control" id="leases_file" placeholder="/var/lib/dhcp/dhcpd.leases" value="' +
+    '<input type="input" class="form-control" id="leases_file" name="leases_file" placeholder="/var/lib/dhcp/dhcpd.leases" value="' +
       glass_config.leases_file +
       '">'
   )
 
   /* Main v4 Config File */
-  input =
-    input +
-    template_render.form_input(
-      'v4 Config File',
-      '<input type="input" class="form-control" id="config_file" placeholder="/etc/dhcp/dhcpd.conf" value="' +
-        glass_config.config_file +
-        '">'
-    )
+  input += template_render.form_input(
+    'v4 Config File',
+    '<input type="input" class="form-control" id="config_file" name="config_file" placeholder="/etc/dhcp/dhcpd.conf" value="' +
+      glass_config.config_file +
+      '">'
+  )
 
   /* Main v6 Config File */
-  input =
-    input +
-    template_render.form_input(
-      'v6 Config File',
-      '<input type="input" class="form-control" id="v6_config_file" placeholder="/etc/dhcp/dhcpd6.conf" value="' +
-        glass_config.v6_config_file +
-        '">'
-    )
+  input += template_render.form_input(
+    'v6 Config File',
+    '<input type="input" class="form-control" id="v6_config_file" name="v6_config_file" placeholder="/etc/dhcp/dhcpd6.conf" value="' +
+      glass_config.v6_config_file +
+      '">'
+  )
 
   /* v4 Pools Config File */
-  input =
-    input +
-    template_render.form_input(
-      'v4 Pools File',
-      '<input type="input" class="form-control" id="v4_pools_file" placeholder="/etc/dhcp/dhcp.d/dhcp4.nets" value="' +
-        glass_config.v4_pools_file +
-        '">'
-    )
+  input += template_render.form_input(
+    'v4 Pools File',
+    '<input type="input" class="form-control" id="v4_pools_file" name="v4_pools_file" placeholder="/etc/dhcp/dhcp.d/dhcp4.nets" value="' +
+      glass_config.v4_pools_file +
+      '">'
+  )
 
   /* v6 Pools Config File */
-  input =
-    input +
-    template_render.form_input(
-      'v6 Pools File',
-      '<input type="input" class="form-control" id="v6_pools_file" placeholder="/etc/dhcp/dhcp.d/dhcp6.nets" value="' +
-        glass_config.v6_pools_file +
-        '">'
-    )
+  input += template_render.form_input(
+    'v6 Pools File',
+    '<input type="input" class="form-control" id="v6_pools_file" name="v6_pools_file" placeholder="/etc/dhcp/dhcp.d/dhcp6.nets" value="' +
+      glass_config.v6_pools_file +
+      '">'
+  )
 
   /* Log File */
-  input =
-    input +
-    template_render.form_input(
-      'Log File',
-      '<input type="input" class="form-control" id="log_file" placeholder="/var/log/dhcp.log" value="' +
-        glass_config.log_file +
-        '">'
-    )
+  input += template_render.form_input(
+    'Log File',
+    '<input type="input" class="form-control" id="log_file" name="log_file" placeholder="/var/log/dhcp.log" value="' +
+      glass_config.log_file +
+      '">'
+  )
+
+  input += '<br>'
+  input += '<br>'
+  input += '<h2>UI Settings</h2>'
 
   /* Header Title */
-  input =
-    input +
-    template_render.form_input(
-      'Header Title (requires application restart)',
-      '<input type="input" class="form-control" id="header_title" placeholder="ISC DHCP Server Interface" value="' +
-        glass_config.header_title +
-        '">'
-    )
+  input += template_render.form_input(
+    'Header Title (requires application restart)',
+    '<input type="input" class="form-control" id="header_title" name="header_title" placeholder="ISC DHCP Server Interface" value="' +
+      glass_config.header_title +
+      '">'
+  )
 
   /* Logo File */
-  input =
-    input +
-    template_render.form_input(
-      'Logo File',
-      '<input type="input" class="form-control" id="logo_file" placeholder="/path/to/file" value="' +
-        glass_config.logo_file +
-        '">'
-    )
+  input += template_render.form_input(
+    'Logo File',
+    `<input type="file" class="form-control" id="logo" name="logo">`
+  )
 
   /* Logo Background Color */
-  input =
-    input +
-    template_render.form_input(
-      'Logo Background Color (hex, keyword, etc)',
-      '<input type="input" class="form-control" id="logo_background_color" placeholder="blue, #e88121, etc" value="' +
-        glass_config.logo_background_color +
-        '">'
-    )
+  input += template_render.form_input(
+    'Logo Background Color (hex, keyword, etc)',
+    '<input type="input" class="form-control" id="logo_background_color" name="logo_background_color" placeholder="blue, #e88121, etc" value="' +
+      glass_config.logo_background_color +
+      '">'
+  )
 
-  input =
-    input +
+  /* Favicon */
+  input += template_render.form_input(
+    'Favicon (Recommended 64x64 or smaller)',
+    `<input type="file" class="form-control" id="favicon" name="favicon">`
+  )
+
+  input +=
     '<br><button type="button" class="btn btn-info waves-effect" onclick="save_config()"><i class="material-icons">settings</i> <span>Save Config</span></button>'
-  input = input + '<br><div id="glass_settings_result"></div>'
+  input += '<br><div id="glass_settings_result"></div>'
 
-  form_data = template_render.form_body('glass-settings-form', input)
+  form_data = template_render.form_body('glass_settings_form', input)
 
   glass_settings_template = template_render.set_template_variable(
     glass_settings_template,
--- routes/glass_settings_save.js
+++ routes/glass_settings_save.js
@@ -5,37 +5,65 @@
 var express = require('express')
 var router = express.Router()
 var authGuard = require('../core/auth-guard.js')
+var multer = require('multer')
 
 var authCheck = authGuard({ groupPermissionLevel: 'admin' })
 
-router.post('/', authCheck, function (req, res, next) {
-  var request = req.body
-  var json_file = require('jsonfile')
-
-  var glass_config = json_file.readFileSync('config/glass_config.json')
-
-  glass_config.leases_file = request.leases_file
-  glass_config.config_file = request.config_file
-  glass_config.v6_config_file = request.v6_config_file
-  glass_config.v4_pools_file = request.v4_pools_file
-  glass_config.v6_pools_file = request.v6_pools_file
-  glass_config.log_file = request.log_file
-  glass_config.header_title = request.header_title
-  glass_config.logo_file = request.logo_file
-  glass_config.logo_background_color = request.logo_background_color
-
-  json_file.writeFile(
-    './config/glass_config.json',
-    glass_config,
-    { spaces: 2 },
-    function (err) {
-      console.error(err)
-    }
-  )
-
-  res.send(
-    '<script type="text/javascript">notification(\'Saved Config!\')</script>'
-  )
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${file.fieldname}.${file.mimetype.split('/')[1]}`)
+  },
 })
+var upload = multer({ storage: storage })
+
+router.post(
+  '/',
+  authCheck,
+  upload.fields([
+    { name: 'logo', maxCount: 1 },
+    { name: 'favicon', maxCount: 1 },
+  ]),
+  function (req, res, next) {
+    var request = req.body
+    var json_file = require('jsonfile')
+
+    var glass_config = json_file.readFileSync('config/glass_config.json')
+
+    glass_config.leases_file = request.leases_file
+    glass_config.config_file = request.config_file
+    glass_config.v6_config_file = request.v6_config_file
+    glass_config.v4_pools_file = request.v4_pools_file
+    glass_config.v6_pools_file = request.v6_pools_file
+    glass_config.log_file = request.log_file
+    glass_config.header_title = request.header_title
+    glass_config.logo_background_color = request.logo_background_color
+
+    // if a logo file was uploaded, set it
+    if (req?.files?.logo) {
+      glass_config.logo_file_path = `../../images/uploads/${req.files.logo[0].filename}`
+    }
+
+    // if a favicon file was uploaded, set it
+    if (req?.files?.favicon) {
+      glass_config.favicon_file_path = `../../images/uploads/${req.files.favicon[0].filename}`
+    }
+
+    json_file.writeFile(
+      './config/glass_config.json',
+      glass_config,
+      { spaces: 2 },
+      function (err) {
+        console.error(err)
+      }
+    )
+
+    res.send(
+      '<script type="text/javascript">notification(\'Saved Config!\')</script>'
+    )
+  }
+)
 
 module.exports = router
