e74b353ef39a9b845ab407a615434fc63c7de0f8
mathewbishop <39043451+mathewbishop@users.noreply.github.com>
2022-11-04 18:39:59 +0000

    - bump version - add display of log file path in UI on log pages

--- routes/dhcp6_log.js
+++ routes/dhcp6_log.js
@@ -7,6 +7,11 @@ var authGuard = require('../core/auth-guard.js')
 var authCheck = authGuard()
 
 router.get('/', authCheck, function (req, res, next) {
+  var json_file = require('jsonfile')
+
+  /* Read Config */
+  glass_config = json_file.readFileSync('config/glass_config.json')
+
   var content = ''
 
   content = template_render.get_template('dhcp6_log')
@@ -16,6 +21,11 @@ router.get('/', authCheck, function (req, res, next) {
     'DHCPv6 Log'
   )
   content = template_render.set_template_variable(content, 'log_content', '')
+  content = template_render.set_template_variable(
+    content,
+    'log_file',
+    glass_config.v6_log_file
+  )
 
   res.send(template_render.get_index_template(content, req.url))
 })
--- routes/dhcp_log.js
+++ routes/dhcp_log.js
@@ -31,11 +31,21 @@ function human_time(time) {
 }
 
 router.get('/', authCheck, function (req, res, next) {
+  var json_file = require('jsonfile')
+
+  /* Read Config */
+  glass_config = json_file.readFileSync('config/glass_config.json')
+
   var content = ''
 
   content = template_render.get_template('dhcp_log')
   content = template_render.set_template_variable(content, 'title', 'DHCP Log')
   content = template_render.set_template_variable(content, 'log_content', '')
+  content = template_render.set_template_variable(
+    content,
+    'log_file',
+    glass_config.log_file
+  )
 
   res.send(template_render.get_index_template(content, req.url))
 })
